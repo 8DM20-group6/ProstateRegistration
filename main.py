eb64b1a1d562dd971491ba1e009fe66f8edf0ee2
Daniel Le <d.le@student.tue.nl>
2024-02-19 13:41:32 +0100

    Added data folder and initial commit main .py files.

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+Atlas-based segmentation prostate
+
+8DM20 - Group 6
+O. Capmany Dalmas, P. Del Popolo, Z. Farshidrokh, D. Le, J. van der Pas, M. Vroemen
+Utrecht University & University of Technology Eindhoven
+
+"""
+
