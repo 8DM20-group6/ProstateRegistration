5222edf096745a8a6636bf2d002240fccc183350
Daniel Le <d.le@student.tue.nl>
2024-02-19 15:34:00 +0100

    Implemented dataset loading and plotting.

--- dataset.py
+++ dataset.py
@@ -7,3 +7,75 @@ Utrecht University & University of Technology Eindhoven
 
 """
 
+import SimpleITK as sitk
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+import os
+import glob
+
+class Dataset():
+    def __init__(self):
+        self.data_path = "data"
+
+    def get_data_paths(self):
+        """
+        Returns all file paths of image and mask from project root directory.
+
+        Returns:
+            list: list of [image, mask] paths.
+        """        
+        filelist = glob.glob(os.path.join(self.data_path, "*"))
+        data_paths = list()
+
+        for data_dir in filelist:
+            image_mhd_path = os.path.join(data_dir, "mr_bffe.mhd")
+            mask_mhd_path = os.path.join(data_dir, "prostaat.mhd")
+            data_paths.append([image_mhd_path, mask_mhd_path])
+        
+        return data_paths
+    
+    def __getitem__(self, index):
+        """Returns array data from image and mask for a given index.
+
+        Args:
+            index (int): index file in dataset
+
+        Returns:
+            list: list of [image, mask] numpy.ndarray 
+        """        
+        data_path = self.get_data_paths()[index]
+        output = list()
+
+        for i, path in enumerate(data_path):
+            itk_image = sitk.ReadImage(path)
+            itk_array = sitk.GetArrayFromImage(itk_image)
+            output.append(itk_array)
+
+        return output
+    
+    def plot(self, index, slice=40):
+        """Plots image, mask and overlay for a given file index.
+
+        Args:
+            index (int): index data file
+            slice (int, optional): index slice. Defaults to 40.
+        """        
+        [image, mask] = self.__getitem__(index)
+        overlay = np.ma.masked_where(mask == 0, mask)
+    
+        fig, axs = plt.subplots(1, 3)
+        axs[0].imshow(image[slice,:,:], cmap="gray")
+        axs[1].imshow(mask[slice,:,:], cmap="gray")
+        axs[2].imshow(image[slice,:,:], cmap="gray")
+        axs[2].imshow(overlay[slice,:,:], cmap="jet", alpha=0.6)
+        titles = ["Image", "Mask", "Overlay"]
+
+        for ax, title in zip(axs, titles):
+            ax.set_title(title)
+            ax.axis("off")
+        fig.suptitle(f"Prostate image #{index}", y=0.85, fontsize=14)
+
+        plt.tight_layout()
+        plt.show()
+
--- main.py
+++ main.py
@@ -6,4 +6,19 @@ O. Capmany Dalmas, P. Del Popolo, Z. Farshidrokh, D. Le, J. van der Pas, M. Vroe
 Utrecht University & University of Technology Eindhoven
 
 """
+# %%
 
+from dataset import Dataset
+# %% Data loading and splitting
+dataset = Dataset()
+print(dataset.get_data_paths())
+dataset.plot(0, slice=40)
+img, mask = dataset[0]
+
+# dataset.plot_data(1, slice=40)
+# dataset.plot_data(3, slice=40)
+
+# %% Registration
+# %% Atlas transformation
+# %% Atlas combining
+# %% Validation
